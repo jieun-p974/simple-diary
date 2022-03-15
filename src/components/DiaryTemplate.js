a7fcf383f7f7cb7deba7fa17baaed44a8e9f31e4
jieun <jieun.p974@gmail.com>
2022-03-15 22:02:21 +0900

    화면 구성

---
+++ src/components/DiaryTemplate.js
@@ -0,0 +1,44 @@
+// diary의 기본적인 형태 component
+import React from "react";
+import styled, { css } from 'styled-components';
+
+const Diary_Template = styled.div`
+    background : #fdefb2;
+    width:512px;
+    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    margin: 0 auto;
+    margin-top: 4rem
+`
+const Title = styled.div`
+    padding: 2rem;
+    font-size: 2.5rem;
+    text-align: ceter;
+    background: #e3c565;
+    color: white;
+`
+const FontWrap = styled.section`
+    padding: 1rem;
+    border-bottom: 1px solid #e3c565;
+`
+const DiaryWrap = styled.section`
+    min-height: 5rem;
+`
+
+// (props)=>{}를 비구조화 할당하여 ({form, children}) => {}로 작성
+const DiaryTemplate = ({form, children}) => {
+    return(
+        <Diary_Template>
+            <Title>
+                일기 제목
+            </Title>    
+            <FontWrap>
+                {form}
+            </FontWrap>
+            <DiaryWrap>
+                {children}
+            </DiaryWrap>
+        </Diary_Template>
+    );
+};
+
+export default DiaryTemplate;
---
+++ src/components/Form.js
@@ -0,0 +1,78 @@
+// input 버튼이 있는 component
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+
+const DiaryForm = styled.div`
+    display: flex;
+`
+const EmotionSelect = styled.select`
+    font-size: 1.25rem;
+    margin-right: 1rem;
+    border-radius: 5px;
+    &:focus {
+        border-color: red;
+    }
+`
+const InputeField = styled.input`
+    flex: 1;
+    font-size: 1.25rem;
+    outline: none;
+    border: none;
+    border-bottom: 1px solid #e3c565;
+`
+const CreateButton =styled.button`
+    padding: 0.5rem 1rem;
+    margin-left: 1rem;
+    background: #e3c565;
+    border-radius: 5px;
+    color: white;
+    font-weight: 600;
+    cursor: pointer;
+    &:hover{
+        background: #dfc98a;
+    }
+`
+
+const Form = () => {
+    const [state, setState] = useState({
+        content:"",
+        emotion: "😀",
+    });
+    const handleChangeState = (e)=>{
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        })
+    }
+    const handleSubmit = () =>{
+        console.log(state);
+        alert("저장성공");
+    }
+    return(
+        <DiaryForm>
+            <EmotionSelect 
+                name='emotion'
+                value={state.emotion}
+                onChange={handleChangeState}
+            >
+                <option value={"happy"}>😀</option>
+                <option value={"smile"}>🙂</option>
+                <option value={"fine"}>😐</option>
+                <option value={"sad"}>😢</option>
+                <option value={"angry"}>😠</option>
+            </EmotionSelect>
+            <InputeField 
+                name='content'
+                value={state.content} 
+                onChange={handleChangeState}
+            />
+            <CreateButton onClick={handleSubmit}>
+                <FontAwesomeIcon icon={faPencil} />
+            </CreateButton>
+        </DiaryForm>
+    );
+}
+
+export default Form;
